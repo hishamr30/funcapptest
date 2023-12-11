d0a021be4123fd632036d46daa834d0ab4de832c
hishamr30 <hisham.nawzer@rackspace.com>
2023-12-11 11:37:04 +0900

    first commit

---
+++ function_app.py
@@ -0,0 +1,21 @@
+import azure.functions as func
+import logging
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.route(route="http_trigger")
+def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+
+        return func.HttpResponse(f"Hello, func app in git hub")
+  
